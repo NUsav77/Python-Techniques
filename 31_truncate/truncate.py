75db3505419f01c39cb37c2a25baa250794ae4c5
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-05-25 16:31:46 -0700

    Update truncate.py

--- 31_truncate/truncate.py
+++ 31_truncate/truncate.py
@@ -24,3 +24,11 @@ def truncate(phrase, n):
         >>> truncate("Woah", 3)
         '...'
     """
+    if n < 3:
+        return "Truncation must be at least 3 characters."
+    elif len(phrase) - 3 < 0:
+        return phrase
+    elif len(phrase) >= n:
+        phrase_update = phrase[:n-3]
+        phrase_update += ('...')
+        return phrase_update
