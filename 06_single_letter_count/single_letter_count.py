a1983b0291b6485f2a3d0f6c7bc26919e6da8478
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-05-25 08:19:15 -0700

    Initial commit

---
+++ 06_single_letter_count/single_letter_count.py
@@ -0,0 +1,17 @@
+def single_letter_count(word, letter):
+    """How many times does letter appear in word (case-insensitively)?
+    
+        >>> single_letter_count('Hello World', 'h')
+        1
+        
+        >>> single_letter_count('Hello World', 'z')
+        0
+        
+        >>> single_letter_count("Hello World", 'l')
+        3
+    """
+
+    low_letter = letter.lower()
+    low_word = word.lower()
+    counter = low_word.count(low_letter)
+    return counter
