b2e121ad895ebff0f88496b5b089eacfa012ff54
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-05-25 14:22:33 -0700

    Update vowel_count.py

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -7,3 +7,14 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+
+    phrase = phrase.lower()
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    vowel_freq = {}
+
+    for char in phrase:
+
+        if char in vowels:
+            vowel_freq[char] = vowel_freq.get(char, 0) + 1
+
+    return vowel_freq
